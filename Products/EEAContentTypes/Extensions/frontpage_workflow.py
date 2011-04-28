d3c9695000fcc3de569df9b9f735282114276362
tiberich <tiberiuichim@users.noreply.github.com>
2011-04-28 13:38:55 +0000

    Refs #8262; Moved package repo location to Products.EEAContentTypes

--- Products/EEAContentTypes/Extensions/frontpage_workflow.py
+++ Products/EEAContentTypes/Extensions/frontpage_workflow.py
@@ -1,11 +1,44 @@
-""" Front page workflow
-"""
+# -*- coding: utf-8 -*-
+#
+# File: EEAContentTypes.py
+#
+# Copyright (c) 2006 by []
+# Generator: ArchGenXML Version 1.5.1-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__author__ = """unknown <unknown>"""
+__docformat__ = 'plaintext'
+
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.WorkflowTool import addWorkflowFactory
 from Products.DCWorkflow.DCWorkflow import DCWorkflowDefinition
 from Products.ExternalMethod.ExternalMethod import ExternalMethod
-from Products.EEAContentTypes.config import PROJECTNAME as productname
+from Products.EEAContentTypes.config import *
+
+##code-section create-workflow-module-header #fill in your manual code here
+##/code-section create-workflow-module-header
+
+
+productname = 'EEAContentTypes'
 
 def setupfrontpage_workflow(self, workflow):
     """Define the frontpage_workflow workflow.
@@ -20,8 +53,11 @@ def setupfrontpage_workflow(self, workflow):
 
     workflow.setProperties(title='frontpage_workflow')
 
-    for s in ['new', 'published', 'webqa_pending',
-              'retracted', 'content_pending', 'proof_reading']:
+    ##code-section create-workflow-setup-method-header #fill in your manual code here
+    ##/code-section create-workflow-setup-method-header
+
+
+    for s in ['new', 'published', 'webqa_pending', 'retracted', 'content_pending', 'proof_reading']:
         workflow.states.addState(s)
 
     for t in ['retract', 'enable', 'publish', 'submit', 'reject']:
@@ -69,15 +105,13 @@ def setupfrontpage_workflow(self, workflow):
                            transitions=['reject', 'publish'])
     stateDef.setPermission('View',
                            0,
-                           ['Manager', 'Owner', 'WebReviewer',
-                            'Reviewer', 'Editor'])
+                           ['Manager', 'Owner', 'WebReviewer', 'Reviewer', 'Editor'])
     stateDef.setPermission('Modify portal content',
                            0,
                            ['Manager', 'WebReviewer'])
     stateDef.setPermission('Access contents information',
                            0,
-                           ['Manager', 'Owner', 'WebReviewer',
-                            'Reviewer', 'Editor'])
+                           ['Manager', 'Owner', 'WebReviewer', 'Reviewer', 'Editor'])
 
     stateDef = workflow.states['retracted']
     stateDef.setProperties(title="""Retracted""",
@@ -94,15 +128,13 @@ def setupfrontpage_workflow(self, workflow):
                            transitions=['submit', 'reject'])
     stateDef.setPermission('View',
                            0,
-                           ['Manager', 'Owner', 'WebReviewer',
-                            'Reviewer', 'Editor'])
+                           ['Manager', 'Owner', 'WebReviewer', 'Reviewer', 'Editor'])
     stateDef.setPermission('Modify portal content',
                            0,
                            ['Manager', 'Reviewer'])
     stateDef.setPermission('Access contents information',
                            0,
-                           ['Manager', 'Owner', 'WebReviewer',
-                            'Reviewer', 'Editor'])
+                           ['Manager', 'Owner', 'WebReviewer', 'Reviewer', 'Editor'])
 
     stateDef = workflow.states['proof_reading']
     stateDef.setProperties(title="""Pending (Proof reading)""",
@@ -186,8 +218,7 @@ def setupfrontpage_workflow(self, workflow):
                                 actbox_name="""reject""",
                                 actbox_url="""""",
                                 actbox_category="""workflow""",
-                                props={
-                                    'guard_roles': 'Owner;Manager;WebReviewer'},
+                                props={'guard_roles': 'Owner;Manager;WebReviewer'},
                                 )
 
     ## State Variable
@@ -195,63 +226,73 @@ def setupfrontpage_workflow(self, workflow):
 
     ## Variables initialization
     variableDef = workflow.variables['review_history']
-    variableDef.setProperties(
-        description="""Provides access to workflow history""",
-        default_value="""""",
-        default_expr="""state_change/getHistory""",
-        for_catalog=0,
-        for_status=0,
-        update_always=0,
-        props={'guard_permissions': 'Request review; Review portal content'})
+    variableDef.setProperties(description="""Provides access to workflow history""",
+                              default_value="""""",
+                              default_expr="""state_change/getHistory""",
+                              for_catalog=0,
+                              for_status=0,
+                              update_always=0,
+                              props={'guard_permissions': 'Request review; Review portal content'})
 
     variableDef = workflow.variables['comments']
-    variableDef.setProperties(
-        description="""Comments about the last transition""",
-        default_value="""""",
-        default_expr="""python:state_change.kwargs.get('comment', '')""",
-        for_catalog=0,
-        for_status=1,
-        update_always=1,
-        props=None)
+    variableDef.setProperties(description="""Comments about the last transition""",
+                              default_value="""""",
+                              default_expr="""python:state_change.kwargs.get('comment', '')""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
     variableDef = workflow.variables['time']
-    variableDef.setProperties(
-        description="""Time of the last transition""",
-        default_value="""""",
-        default_expr="""state_change/getDateTime""",
-        for_catalog=0,
-        for_status=1,
-        update_always=1,
-        props=None)
+    variableDef.setProperties(description="""Time of the last transition""",
+                              default_value="""""",
+                              default_expr="""state_change/getDateTime""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
     variableDef = workflow.variables['actor']
-    variableDef.setProperties(
-        description="""The ID of the user who performed the last transition""",
-        default_value="""""",
-        default_expr="""user/getId""",
-        for_catalog=0,
-        for_status=1,
-        update_always=1,
-        props=None)
+    variableDef.setProperties(description="""The ID of the user who performed the last transition""",
+                              default_value="""""",
+                              default_expr="""user/getId""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
     variableDef = workflow.variables['action']
-    variableDef.setProperties(
-        description="""The last transition""",
-        default_value="""""",
-        default_expr="""transition/getId|nothing""",
-        for_catalog=0,
-        for_status=1,
-        update_always=1,
-        props=None)
+    variableDef.setProperties(description="""The last transition""",
+                              default_value="""""",
+                              default_expr="""transition/getId|nothing""",
+                              for_catalog=0,
+                              for_status=1,
+                              update_always=1,
+                              props=None)
 
-def createfrontpage_workflow(self, wfid):
+    ## Worklists Initialization
+
+
+    # WARNING: below protected section is deprecated.
+    # Add a tagged value 'worklist' with the worklist name to your state(s) instead.
+
+    ##code-section create-workflow-setup-method-footer #fill in your manual code here
+    ##/code-section create-workflow-setup-method-footer
+
+
+
+def createfrontpage_workflow(self, id):
     """Create the workflow for EEAContentTypes.
     """
 
-    ob = DCWorkflowDefinition(wfid)
+    ob = DCWorkflowDefinition(id)
     setupfrontpage_workflow(self, ob)
     return ob
 
 addWorkflowFactory(createfrontpage_workflow,
                    id='frontpage_workflow',
                    title='frontpage_workflow')
+
+##code-section create-workflow-module-footer #fill in your manual code here
+##/code-section create-workflow-module-footer
+
--- Products/EEAContentTypes/tests/testRelations.py
+++ Products/EEAContentTypes/tests/testRelations.py
@@ -1,5 +1,29 @@
-""" Tests for Relations
-"""
+# -*- coding: utf-8 -*-
+#
+# File: testFrontpage.py
+#
+# Copyright (c) 2006 by []
+# Generator: ArchGenXML Version 1.5.1-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
 from Products.EEAContentTypes.tests.base import EEAContentTypeTestCase
 from Products.EEAContentTypes.interfaces import IRelations
 from eea.themecentre.interfaces import IThemeTagging
@@ -9,13 +33,10 @@ class TestRelations(EEAContentTypeTestCase):
     """ Test-cases for class(es) relations. """
 
     def afterSetUp(self):
-        """ Set up
-        """
         setSite(self.portal)
         self.setRoles('Member')
         self.workflow = self.portal.portal_workflow
-        self.portal.acl_users._doAddUser(
-            'reviewer', 'secret', ['Reviewer'], [])
+        self.portal.acl_users._doAddUser('reviewer', 'secret', ['Reviewer'], [])
 
         self.folder.invokeFactory('Document', id='unpub_air')
         self.folder.invokeFactory('Highlight', id='unpub_air_high')
@@ -29,7 +50,7 @@ class TestRelations(EEAContentTypeTestCase):
         self.folder.invokeFactory('Article', id='rel_not_same_type')
         self.folder.invokeFactory('Article', id='art1_pub')
         self.folder.invokeFactory('Article', id='art2_pub')
-        self.folder.invokeFactory('Article', id='art3')
+        self.folder.invokeFactory('Article', id='art3')        
 
         IThemeTagging(self.folder.unpub_air).tags = ['air']
         IThemeTagging(self.folder.unpub_air_high).tags = ['air']
@@ -46,8 +67,8 @@ class TestRelations(EEAContentTypeTestCase):
         self.workflow.doActionFor(self.folder.pub_air, 'publish')
         self.workflow.doActionFor(self.folder.pub, 'publish')
         self.workflow.doActionFor(self.folder.art1_pub, 'publish')
-        self.workflow.doActionFor(self.folder.art2_pub, 'publish')
-
+        self.workflow.doActionFor(self.folder.art2_pub, 'publish')        
+        
         self.folder.unpub_air.reindexObject()
         self.folder.unpub_air_high.reindexObject()
         self.folder.unpub.reindexObject()
@@ -57,27 +78,22 @@ class TestRelations(EEAContentTypeTestCase):
 
         # make all themes public (non deprecated)
         for theme in self.portal.portal_vocabularies.themes.objectValues():
-            self.portal.portal_workflow.doActionFor(theme, 'publish')
+            self.portal.portal_workflow.doActionFor(theme, 'publish') 
 
     def testAnonymous(self):
-        """ Anonymous
-        """
         self.login('reviewer')
         related = IRelations(self.folder.pub_air).byTheme()
         ids = sorted([obj.getId() for obj in related])
         # we expect the objects with theme 'air' to be found
-        self.assertEquals(ids, ['priv_air_press',
-                                'unpub_air', 'unpub_air_high'])
+        self.assertEquals(ids, ['priv_air_press', 'unpub_air', 'unpub_air_high'])
 
         context = self.folder.pub_air
         related = IRelations(context).byTheme(portal_type=context.portal_type)
-        ids = sorted(obj.getId() for obj in related)
+        ids = sorted([obj.getId() for obj in related])
         # we expect the Document objects with theme 'air' to be found
         self.assertEquals(ids, ['unpub_air'])
 
     def testAuthenticated(self):
-        """ Authenitcated
-        """
         self.login('test_user_1_')
         self.setRoles('Member')
 
@@ -85,82 +101,63 @@ class TestRelations(EEAContentTypeTestCase):
         ids = sorted([obj.getId() for obj in related])
         # all objects that the user has permission to view should be
         # found, even private ones
-        self.assertEquals(ids, ['priv_air_press',
-                                'unpub_air', 'unpub_air_high'])
+        self.assertEquals(ids, ['priv_air_press', 'unpub_air', 'unpub_air_high'])
 
         constraint = {'review_state': 'published'}
         related = IRelations(self.folder.unpub_air).byTheme(
                 constraints=constraint)
-        ids = sorted(obj.getId() for obj in related)
+        ids = sorted([obj.getId() for obj in related])
         # all public objects that the user has permission to view should be
         # found
         self.assertEquals(ids, ['pub_air'])
 
     def testForwardReferences(self):
-        """ Forward references
-        """
         related = IRelations(self.folder.rel_from).forwardReferences()
         ids = sorted([obj.getId() for obj in related])
-        self.assertEquals(ids, ['rel_not_same_type', 'rel_to',
-                                'rel_to2', 'unpub_air'])
+        self.assertEquals(ids, ['rel_not_same_type', 'rel_to', 'rel_to2', 'unpub_air'])
 
         related = IRelations(self.folder.rel_to).forwardReferences()
-        ids = sorted(obj.getId() for obj in related)
+        ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, [])
 
     def testAutoContext(self):
-        """ Auto context
-        """
         related = IRelations(self.folder.rel_to).autoContextReferences()
         ids = sorted([obj.getId() for obj in related])
-        self.assertEquals(ids, ['rel_from', 'rel_not_same_type',
-                                'rel_to2', 'unpub_air'])
+        self.assertEquals(ids, ['rel_from', 'rel_not_same_type', 'rel_to2', 'unpub_air'])
 
-        related = IRelations(self.folder.rel_to).autoContextReferences(
-            portal_type=self.folder.rel_to.portal_type)
-        ids = sorted(obj.getId() for obj in related)
+        related = IRelations(self.folder.rel_to).autoContextReferences(portal_type=self.folder.rel_to.portal_type)
+        ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, ['rel_from', 'rel_to2', 'unpub_air'])
 
     def testBackReferences(self):
-        """ Back references
-        """
         related = IRelations(self.folder.rel_to).backReferences()
         ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, ['rel_from', 'unpub_air'])
 
         related = IRelations(self.folder.rel_from).backReferences()
-        ids = sorted(obj.getId() for obj in related)
+        ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, [])
 
     def testAllRelations(self):
-        """ All relations
-        """
         constraint = {'review_state': 'published'}
         related = IRelations(self.folder.unpub_air).all(constraints=constraint)
-        ids = sorted(obj.getId() for obj in related)
+        ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, ['pub_air', 'rel_from', 'rel_to'])
 
     def testReferences(self):
-        """ References
-        """
         related = IRelations(self.folder.unpub_air).references()
         ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, ['rel_from', 'rel_to'])
 
     def testPublicationGroup(self):
-        """ Publication group
-        """
         related = IRelations(self.folder.art1_pub).byPublicationGroup()
         ids = sorted([obj.getId() for obj in related])
-        self.assertEquals(ids, ['art2_pub', 'art3', 'rel_not_same_type'])
+        self.assertEquals(ids, ['art2_pub', 'art3', 'rel_not_same_type'])        
 
-        related = IRelations(self.folder.art2_pub).byPublicationGroup(
-            constraints = {'review_state': 'published'})
-        ids = sorted(obj.getId() for obj in related)
+        related = IRelations(self.folder.art2_pub).byPublicationGroup(constraints = {'review_state': 'published'})
+        ids = sorted([obj.getId() for obj in related])
         self.assertEquals(ids, ['art1_pub'])
-
+        
 def test_suite():
-    """ Suite
-    """
     import unittest
     return  unittest.TestSuite(unittest.makeSuite(TestRelations))
