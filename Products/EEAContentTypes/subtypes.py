f6b6223feb95e5a9287cde52e37897e9d6f8afc8
tiberich <tiberiuichim@users.noreply.github.com>
2011-04-28 13:38:55 +0000

    Refs #8262; Moved package repo location to Products.EEAContentTypes

--- Products/EEAContentTypes/subtypes.py
+++ Products/EEAContentTypes/subtypes.py
@@ -1,14 +1,14 @@
-from Products.Archetypes.interfaces import IBaseContent
-from Products.Archetypes.interfaces import ISchema
-from archetypes.schemaextender.field import ExtensionField
 from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.field import ExtensionField
 from eea.geotags import field
 from eea.geotags import widget
-from zope.component import adapts
-from zope.interface import Interface, implements
+from zope.interface import implements
 
-#from Products.ATContentTypes.interface import IATContentType
+from zope.interface import Interface, implements
+from zope.component import adapts
 from p4a.video.interfaces import IVideoEnhanced
+from Products.Archetypes.interfaces import ISchema
+from Products.Archetypes.interfaces import IBaseContent
 
 
 class ExtensionGeotagsSinglefield(ExtensionField, field.GeotagsStringField):
@@ -21,20 +21,18 @@ class ExtensionGeotagsMultifield(ExtensionField, field.GeotagsLinesField):
 
 class BaseContentSchemaExtender(object):
     implements(ISchemaExtender)
-    adapts(IVideoEnhanced)
-    #adapts(IBaseContent)
+    adapts(IBaseContent)
 
     fields =  [
-            ExtensionGeotagsSinglefield(
-                name='location',
-                schemata='categorization',
-                default='',
-                widget=widget.GeotagsWidget(
-                    label='Location',
-                    description='Single geographical location'
-                    )
-                )
-            ]
+        ExtensionGeotagsSinglefield(
+    		name='location',
+                schemata='geographical coverage',
+    		widget=widget.GeotagsWidget(
+        	    label='Location',
+        	    description='Single geographical location'
+    		)
+            )
+    ]
 
     def __init__(self, context):
         self.context = context
@@ -45,34 +43,33 @@ class BaseContentSchemaExtender(object):
 
 class GeotagMixinEdit(object):
     adapts(IBaseContent)
-
+    
     def __init__(self, context):
-        self.context = context
+	self.context = context
 
     def geotag():
-        def get(self):
+	def get(self):
             schema = ISchema(self.context)
             field = schema['location']
             accessor = field.getAccessor(self.context)
             return accessor()
-
-        def set(self, value):
+    	    
+	def set(self, value):
             schema = ISchema(self.context)
             field = schema['location']
             mutator = field.getMutator(self.context)
             mutator(value)
-
-        return property(get, set)
-
+	    
+	return property(get, set)
     geotag = geotag()
 
 
 class IGeotagSingleEdit(Interface):
     """Interface for edit single location"""
     geotag = widget.location.GeotagSingleField(
-            title = u"Geotag",
-            description=u"The location of the video"
-            )
+	title = u"Geotag",
+	description=u"The location of the video"
+	)
 
 
 class GeotagSingleEdit(GeotagMixinEdit):
@@ -81,13 +78,13 @@ class GeotagSingleEdit(GeotagMixinEdit):
 
 class IGeotagMultiEdit(Interface):
     """Interface for edit multiple locations"""
-
     geotag = widget.location.GeotagMultiField(
-            title = u"Geotag",
-            description=u"The locations of the video"
-            )
+	title = u"Geotag",
+	description=u"The locations of the video"
+	)
 
-
+	
 class GeotagMultiEdit(GeotagMixinEdit):
     implements(IGeotagMultiEdit)
+
 
