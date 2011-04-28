d797f466c326337e739a257b3ea8d61fccb14612
tiberich <tiberiuichim@users.noreply.github.com>
2011-04-28 13:38:55 +0000

    Refs #8262; Moved package repo location to Products.EEAContentTypes

--- Products/EEAContentTypes/Extensions/eea_public_approval_workflow_scripts.py
+++ Products/EEAContentTypes/Extensions/eea_public_approval_workflow_scripts.py
@@ -1,15 +1,15 @@
 from Products.CMFCore.utils import getToolByName
 
 def publishContent(self,state_change, **kw):
-    obj = state_change.object
-    portalType = obj.portal_type
-    oid = obj.getId()
+    object = state_change.object
+    portalType = object.portal_type
+    id = object.getId()
     if portalType == 'QuickEvent':
-        srcFldr = obj.aq_parent
+        srcFldr = object.aq_parent
         dstFldr = srcFldr.aq_parent
-        objs = srcFldr.manage_cutObjects([oid,])
+        objs = srcFldr.manage_cutObjects([id,])
         dstFldr.manage_pasteObjects(objs)
-        new_obj = dstFldr[oid]
+        new_obj = dstFldr[id]
         new_obj.setLayout('event_view')
         raise state_change.ObjectMoved(new_obj, new_obj)
 
@@ -17,10 +17,10 @@ def publishContent(self,state_change, **kw):
     
 def submitContent(self, state_change, **kw):
     """ DEPRECATED """
-    obj = state_change.object
+    object = state_change.object
     mhost = self.MailHost
     props = getToolByName(self, 'portal_properties').workflow_properties
-    portalType = obj.portal_type.lower()
+    portalType = object.portal_type.lower()
     defaultEmails = getattr(props, 'default_webqa', ['sasha.vincic@eea.europa.eu'])
     toEmail = list(getattr(props, self.portalType + '_webqa', defaultEmails))
     fromEmail = "%s <%s>" % (self.email_from_name, self.email_from_address)
@@ -42,11 +42,11 @@ def submitContent(self, state_change, **kw):
     EEA web team
     """
 
-    #absObjUrl = object.absolute_url()
+    absObjUrl = object.absolute_url()
     objUrl = object.absolute_url(1)
     cmsUrl = getattr(props, 'cms_url', 'https://www-cms.eea.europa.eu/SITE/')
     editUrl = cmsUrl + objUrl + '/edit'
-    #publishUrl = cmsUrl + objUrl + '/content_status_modify?workflow_action=publish'
+    publishUrl = cmsUrl + objUrl + '/content_status_modify?workflow_action=publish'
     msg = message % (
           portalType,
           object.Title(),
