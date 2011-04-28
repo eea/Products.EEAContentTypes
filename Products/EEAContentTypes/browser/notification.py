e005577cfc754c8a3f66fbf7d7da74f4ac370961
tiberich <tiberiuichim@users.noreply.github.com>
2011-04-28 13:38:55 +0000

    Refs #8262; Moved package repo location to Products.EEAContentTypes

--- Products/EEAContentTypes/browser/notification.py
+++ Products/EEAContentTypes/browser/notification.py
@@ -1,7 +1,7 @@
-""" Send as notification """
-
 from Products.CMFCore.utils import getToolByName
+#from Products.PloneGazette.Newsletter import Newsletter
 from Products.CMFPlone import PloneMessageFactory as _
+
 
 class SendAsNotification(object):
     """ Send the context as email notification """
@@ -20,33 +20,25 @@ class SendAsNotification(object):
 
         #Get notification center
         #TODO: to be changed if we use multiple notification centers
-        res = cat.searchResults(portal_type = 'NewsletterTheme',
-                                getId='eea_main_subscription')
-        notif_center = res[0].getObject()
+        res = cat.searchResults(portal_type = 'NewsletterTheme')[0]
+        notif_center = res.getObject()
 
         #Check if notification already generated
         for nl in notif_center.getNewsletters():
             relItem = getattr(nl, 'relatedItem', None)
             if relItem and obj_uid == relItem:
                 status = wf.getInfoFor(nl, 'review_state', None)
-                if status == 'published':
-                    status = \
-                     '<span style="color:green">notification was sent</span>'
-                else:
-                    status = \
-                     '<span style="color:red">notification not yet sent</span>'
-                message = _(u'Email notification already generated. Please check under <a title="Notification center" href="${url}/folder_contents">notification center</a>. Status: ${status}',
-                            mapping = {u'url' : notif_center.absolute_url(),
-                                       u'status' : status})
-                pu.addPortalMessage(message, 'structure')
-                return self.request.RESPONSE.redirect(
-                    self.context.absolute_url())
+            if status == 'published':
+                status = '<span style="color:green">notification was sent</span>'
+            else:
+                status = '<span style="color:red">notification not yet sent</span>'
+            message = _(u'Email notification already generated. Please check under <a title="Notification center" href="${url}/folder_contents">notification center</a>. Status: ${status}', 
+            mapping = {u'url' : notif_center.absolute_url(), u'status' : status})
+            pu.addPortalMessage(message, 'structure')
+            return self.request.RESPONSE.redirect(self.context.absolute_url())
 
         #Create email notification object
-        notif_center.invokeFactory('Newsletter',
-                                   id=obj_id,
-                                   title=obj_title,
-                                   relatedItem=obj_uid)
+        notif_center.invokeFactory('Newsletter',id=obj_id, title=obj_title, relatedItem=obj_uid)
         message = _(u'Email notification object created. Check under <a title="Notification center" href="${url}/folder_contents">notification center</a> if you want to be sent',
                 mapping={u'url' : notif_center.absolute_url()})
         pu.addPortalMessage(message, 'structure')
