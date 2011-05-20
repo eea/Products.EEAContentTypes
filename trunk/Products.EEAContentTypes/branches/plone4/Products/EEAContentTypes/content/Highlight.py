b98dd547bcc24993d33ab97be03f2f48c79ec0d0
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-20 13:08:57 +0000

    Refs #8262; fixes testSyndication

--- trunk/Products.EEAContentTypes/branches/plone4/Products/EEAContentTypes/content/Highlight.py
+++ trunk/Products.EEAContentTypes/branches/plone4/Products/EEAContentTypes/content/Highlight.py
@@ -29,42 +29,26 @@
 __author__ = """unknown <unknown>"""
 __docformat__ = 'plaintext'
 
-from zope.interface import implements
 from AccessControl import ClassSecurityInfo
-from Products.EEAContentTypes.content.ExternalHighlight import ExternalHighlight
 from Products.ATContentTypes.content.newsitem import ATNewsItem
-from Products.EEAContentTypes.config import PROJECTNAME
-from Products.EEAContentTypes.content.quotation import quotation_schema
-
-##code-section module-header #fill in your manual code here
-from Products.CMFCore.permissions import ModifyPortalContent
-from eea.themecentre.interfaces import IThemeTagging
-from Products.EEAContentTypes.content.ExternalHighlight import schema as ExtHighlightSchema
 from Products.Archetypes.Schema import getNames
-try:
-    from Products.LinguaPlone.public import Schema, registerType 
-    Schema, registerType 
-except ImportError:
-    # No multilingual support
-    from Products.Archetypes.public import Schema, registerType 
-##/code-section module-header
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.EEAContentTypes.config import PROJECTNAME
+from Products.EEAContentTypes.content.ExternalHighlight import ExternalHighlight
+from Products.EEAContentTypes.content.ExternalHighlight import schema as ExtHighlightSchema
+from Products.EEAContentTypes.content.interfaces import IExternalHighlight
+from Products.EEAContentTypes.content.quotation import quotation_schema
+from Products.LinguaPlone.public import Schema, registerType 
+from eea.themecentre.interfaces import IThemeTagging
+from zope.interface import implements
 
-from interfaces import IExternalHighlight
+schema = Schema((),)
 
-schema = Schema((
-
-),
-)
-
-##code-section after-local-schema #fill in your manual code here
-##/code-section after-local-schema
 
 Highlight_schema =  getattr(ATNewsItem, 'schema', Schema(())).copy() + \
     getattr(ExternalHighlight, 'schema', Schema(())).copy() + \
     quotation_schema.copy() + \
     schema.copy()
-
-##code-section after-schema #fill in your manual code here
 
 # put all the external highlights fields into their own schema
 ExternalHighlightSchema = ExtHighlightSchema.copy()
@@ -79,38 +63,19 @@ for fieldname in getNames(ExternalHighlightSchema):
 
 Highlight_schema['text'].required = True
 Highlight_schema.moveField('image', before='imageCaption')
-##/code-section after-schema
+
 
 class Highlight(ExternalHighlight, ATNewsItem):
     """<p>Hightlights are links with descriptions presented as news on
     the website.</p>
     """
-    security = ClassSecurityInfo()
-    __implements__ = (getattr(ExternalHighlight,'__implements__',()),) + (getattr(ATNewsItem,'__implements__',()),)
     implements(IExternalHighlight)
-
-    # This name appears in the 'add' box
     archetype_name = 'Highlight'
-
-    meta_type = 'Highlight'
-    portal_type = 'Highlight'
-    allowed_content_types = [] + list(getattr(ExternalHighlight, 'allowed_content_types', [])) + list(getattr(ATNewsItem, 'allowed_content_types', []))
-    filter_content_types = 1
-    global_allow = 1
-    immediate_view = 'base_view'
-    default_view = 'highlight_view'
-    suppl_views = ()
-    typeDescription = "Highlight"
-    typeDescMsgId = 'description_edit_highlight'
+    schema = Highlight_schema
 
     _at_rename_after_creation = True
 
-    schema = Highlight_schema
-
-    ##code-section class-header #fill in your manual code here
-    content_icon   = 'highlight_icon.gif'
-    ##/code-section class-header
-
+    security = ClassSecurityInfo()
 
     security.declarePublic('getPublishDate')
     def getPublishDate(self):
@@ -134,10 +99,3 @@ class Highlight(ExternalHighlight, ATNewsItem):
 
 
 registerType(Highlight, PROJECTNAME)
-# end of class Highlight
-
-##code-section module-footer #fill in your manual code here
-##/code-section module-footer
-
-
-
