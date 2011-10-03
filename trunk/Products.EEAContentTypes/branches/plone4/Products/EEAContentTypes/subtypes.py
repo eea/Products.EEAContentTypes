0ee71a8b5bc6775c58c5c3477f63406a2b0971ce
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-03 11:39:35 +0000

    Refs #8567; allow hiding the subtyper menu based on context portal type; renamed Video Container subtype to Video Topic Container for ATTopic

--- trunk/Products.EEAContentTypes/branches/plone4/Products/EEAContentTypes/subtypes.py
+++ trunk/Products.EEAContentTypes/branches/plone4/Products/EEAContentTypes/subtypes.py
@@ -6,9 +6,12 @@ from eea.geotags import field
 from eea.geotags import widget
 from p4a.subtyper.engine import Subtyper as BaseSubtyper, DescriptorWithName
 from p4a.subtyper.interfaces import IPossibleDescriptors, IPortalTypedPossibleDescriptors
+from p4a.video.subtype import TopicVideoContainerDescriptor as \
+        BaseTopicVideoContainerDescriptor, _
 from zope.component import adapts
 from zope.component import queryAdapter
 from zope.interface import Interface, implements
+
 
 class ExtensionGeotagsSinglefield(ExtensionField, field.GeotagsStringField):
     """ derivative of blobfield for extending schemas """
@@ -96,13 +99,26 @@ class Subtyper(BaseSubtyper):
     possible_types implementation. The default implementation, due to
     adapter resolution order, doesn't take into account adapters for
     IPortalTypedPossibleDescriptors
+
+    Also, not it's possible to hide the menu for content types where
+    it doesn't make sense. This is due to the fact that the subtyper 
+    in p4a.video is registered for somewhat generic interfaces 
+    (for ex: IATFolder)
     """
+    _skip = ('Data', 'EEAFigure')
 
     def possible_types(self, obj):
+        if obj.portal_type in self._skip:
+            return []
+
         possible = queryAdapter(obj, IPortalTypedPossibleDescriptors)
         if possible and possible.possible:
             return (DescriptorWithName(n, c) for n, c in possible.possible)
 
         possible = IPossibleDescriptors(obj)
         return (DescriptorWithName(n, c) for n, c in possible.possible)
+
+
+class TopicVideoContainerDescriptor(BaseTopicVideoContainerDescriptor):
+    title = _("Video Topic Container")
 
