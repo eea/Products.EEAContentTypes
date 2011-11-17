5833f8b6324c1e444df0b0cc285cb3d709a4f6de
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-17 16:10:51 +0000

    Only include languages which have a translated eea homepage.

--- trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/browser/language.py
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/browser/language.py
@@ -61,12 +61,12 @@ class Languages(BrowserView):
         site = getattr(portal, 'SITE', None)
         for lang in languages:
             langcode = lang[0]
-            url = 'http://local.%s.eea.europa.eu' % langcode
+            url = 'http://www.eea.europa.eu/%s' % langcode
             if site is not None:
                 localSite = site.getTranslation(langcode)
                 if localSite is not None:
                     url = localSite.absolute_url()
-            sites.append( { 'lang' : lang[1],
+                    sites.append( { 'lang' : lang[1],
                             'langcode' : langcode,
                             'url' : url } )
         return sites
