bb904974ffbad7261dd2af57ac36876e8ebb44cf
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-21 16:21:07 +0000

    Tagged Products.EEAContentTypes 4.1.

---
+++ trunk/Products.EEAContentTypes/tags/4.1/Products/EEAContentTypes/content/CFTRequestor.py
@@ -0,0 +1,225 @@
+""" CFTRequestor """
+
+# -*- coding: utf-8 -*-
+#
+# File: CFTRequestor.py
+#
+# Copyright (c) 2006 by []
+# Generator: ArchGenXML Version 1.5.1-svn
+#            http://plone.org/products/archgenxml
+#
+# GNU General Public License (GPL)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+
+__docformat__ = 'plaintext'
+
+from AccessControl import ClassSecurityInfo
+from Products.ATContentTypes.content.base import ATCTContent
+from Products.ATVocabularyManager.namedvocabulary import NamedVocabulary
+from Products.Archetypes.atapi import Schema, StringField, StringWidget
+from Products.Archetypes.atapi import SelectionWidget, registerType
+from Products.CMFCore.utils import getToolByName
+from Products.EEAContentTypes.config import  PROJECTNAME
+from Products.validation.validators import ExpressionValidator
+from interfaces import ICFTRequestor
+from zope.component import getMultiAdapter
+from zope.event import notify
+from zope.interface import implements
+from zope.lifecycleevent import ObjectModifiedEvent
+import transaction
+
+
+schema = Schema((
+
+    StringField(
+        name='organisation',
+        widget=StringWidget(
+            label="Organisation",
+            label_msgid='EEAContentTypes_label_organisation',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1
+    ),
+
+    StringField(
+        name='address1',
+        widget=StringWidget(
+            label="Address 1",
+            label_msgid='EEAContentTypes_label_address1',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1
+    ),
+
+    StringField(
+        name='address2',
+        widget=StringWidget(
+            label="Address 2",
+            label_msgid='EEAContentTypes_label_address2',
+            i18n_domain='EEAContentTypes',
+        )
+    ),
+
+    StringField(
+        name='city',
+        City="City",
+        widget=StringWidget(
+            label='City',
+            label_msgid='EEAContentTypes_label_city',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1
+    ),
+
+    StringField(
+        name='postCode',
+        widget=StringWidget(
+            label="Post Code",
+            label_msgid='EEAContentTypes_label_postCode',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1
+    ),
+
+    StringField(
+        name='country',
+        widget=SelectionWidget(
+            label="Country",
+            label_msgid='EEAContentTypes_label_country',
+            i18n_domain='EEAContentTypes',
+        ),
+        enforceVocabulary=1,
+        vocabulary=NamedVocabulary("""countries"""),
+        default="default",
+        validators=(ExpressionValidator('''python:value != "default"'''),),
+        required=1
+    ),
+
+    StringField(
+        name='phone',
+        widget=StringWidget(
+            label="Phone Number",
+            label_msgid='EEAContentTypes_label_phone',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1
+    ),
+
+    StringField(
+        name='fax',
+        widget=StringWidget(
+            label="Fax Number",
+            label_msgid='EEAContentTypes_label_fax',
+            i18n_domain='EEAContentTypes',
+        )
+    ),
+
+    StringField(
+        name='email',
+        widget=StringWidget(
+            label="Email Address",
+            label_msgid='EEAContentTypes_label_email',
+            i18n_domain='EEAContentTypes',
+        ),
+        required=1,
+        validators=('isEmail',)
+    ),
+
+    StringField(
+        name='remoteHost',
+        widget=StringWidget(
+            label="Remote Host",
+            visible={'edit': 'invisible'},
+            label_msgid='EEAContentTypes_label_remoteHost',
+            i18n_domain='EEAContentTypes',
+        )
+    ),
+
+    StringField(
+        name='remoteAddr',
+        widget=StringWidget(
+            label="Remote Address",
+            visible={'edit': 'invisible'},
+            label_msgid='EEAContentTypes_label_remoteAddr',
+            i18n_domain='EEAContentTypes',
+        )
+    ),
+
+),
+)
+
+CFTRequestor_schema = getattr(ATCTContent, 'schema', Schema(())).copy() + \
+    schema.copy()
+
+CFTRequestor_schema['description'].schemata = 'metadata'
+CFTRequestor_schema['title'].widget.label = 'Name'
+
+class CFTRequestor(ATCTContent):
+    """
+    """
+    security = ClassSecurityInfo()
+    __implements__ = (getattr(ATCTContent, '__implements__', ()),)
+
+    # This name appears in the 'add' box
+    archetype_name = 'CFTRequestor'
+
+    meta_type = 'CFTRequestor'
+    portal_type = 'CFTRequestor'
+    allowed_content_types = [] + list(getattr(ATCTContent, 'allowed_content_types', []))
+    filter_content_types = 0
+    global_allow = 0
+    #content_icon = 'CFTRequestor.gif'
+    immediate_view = 'base_view'
+    default_view = 'cftrequestor_anon_view'
+    suppl_views = ()
+    typeDescription = "CFTRequestor"
+    typeDescMsgId = 'description_edit_cftrequestor'
+
+    _at_rename_after_creation = True
+
+    schema = CFTRequestor_schema
+
+    implements(ICFTRequestor)
+
+    security.declarePrivate('_renameAfterCreation')
+    def _renameAfterCreation(self, check_auto_id=False):
+        """Renames an requestor like its UID.
+        """
+        transaction.savepoint(optimistic=True)
+        new_id = '%s' % self.UID()
+        self.setId(new_id)
+        return new_id
+
+    def objectModified(self):
+        notify(ObjectModifiedEvent(self))
+
+    def reindexObject(self, **kw):
+        pass
+
+registerType(CFTRequestor, PROJECTNAME)
+
+
+def submit_requestor(obj, event):
+    valid = getMultiAdapter((obj, obj.REQUEST), name='isValid')
+    if valid.validate():
+        obj.setRemoteAddr(obj.REQUEST.get('REMOTE_ADDR'))
+        wf = getToolByName(obj, 'portal_workflow')
+        actions = wf.getTransitionsFor(obj)
+        if len(actions) > 0:
+            wf.doActionFor(obj, 'submit')
+
---
+++ trunk/Products.EEAContentTypes/tags/4.1/Products/EEAContentTypes/subtypes.py
@@ -0,0 +1,210 @@
+""" Subtypes
+"""
+from Products.Archetypes.interfaces import IBaseContent
+from Products.Archetypes.interfaces import ISchema
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from eea.themecentre.content.ThemeTaggable import ThemesField
+from eea.indicators.content.interfaces import IExternalDataSpec
+from eea.geotags import field
+from eea.geotags import widget
+from p4a.subtyper.engine import Subtyper as BaseSubtyper, DescriptorWithName
+from p4a.subtyper.interfaces import (
+    IPossibleDescriptors,
+    IPortalTypedPossibleDescriptors
+)
+from p4a.video.subtype import TopicVideoContainerDescriptor as \
+        BaseTopicVideoContainerDescriptor, _
+from zope.component import adapts
+from zope.component import queryAdapter
+from zope.interface import Interface, implements
+from Products.LinguaPlone.public import StringField
+from Products.LinguaPlone.public import InAndOutWidget
+
+
+class ExtensionStringField(ExtensionField, StringField):
+    """ derivative of stringfield for extending schemas """
+
+class ExtensionGeotagsSinglefield(ExtensionField, field.GeotagsStringField):
+    """ derivative of blobfield for extending schemas """
+
+class ExtensionGeotagsMultifield(ExtensionField, field.GeotagsLinesField):
+    """ derivative of blobfield for extending schemas """
+
+class ExtensionThemesField(ExtensionField, ThemesField):
+    """ derivative of themesfield for extending schemas """
+
+class LocationSchemaExtender(object):
+    """ Extends base schema with extra fields.
+    To be used for base content class. """
+    implements(ISchemaExtender)
+
+    fields =  [
+            ExtensionGeotagsMultifield(
+                name='location',
+                schemata='categorization',
+                widget=widget.GeotagsWidget(
+                    label='Geotags / Locations',
+                    description=('Geotags: multiple geographical locations '
+                                 'related to this content.')
+                    )
+                )
+            ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        """ Fields
+        """
+        return self.fields
+
+class ThemesSchemaExtender(object):
+    """ Extends schema with themes field
+    """
+    implements(ISchemaExtender)
+
+    fields = [
+        ExtensionThemesField(
+            name='themes',
+            schemata='categorization',
+            validators=('maxValues',),
+            widget=InAndOutWidget(
+                maxValues=3,
+                label="Themes",
+                description="Choose max 3 themes",
+                ),
+            languageIndependent=True,
+            vocabulary_factory=u"Allowed themes for edit",
+        ),
+    ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        """ Fields
+        """
+        return self.fields
+
+class RequiredSchemaModifier(object):
+    """ Modify schema
+    """
+    implements(ISchemaModifier)
+
+    def __init__(self, context):
+        self.context = context
+
+    def fiddle(self, schema):
+        """ Modify schema
+        """
+        portal_type = getattr(self.context, 'portal_type', '')
+
+        #TODO Move this to zcml
+        if portal_type not in [
+            'Article', 'Highlight', 'PressRelease', 'Speech',
+            'DiversityReport', 'Data', 'EEAFigure', 'EcoTip',
+            'EyewitnessStory', 'GIS Application', 'Document',
+            'PolicyDocumentReference', 'Report', 'SOERKeyFact',
+            'SOERMessage', 'File']:
+            return
+
+        if 'subject' in schema:
+            schema['subject'].required = True
+        if 'location' in schema:
+            schema['location'].required = True
+
+        if 'themes' in schema:
+            if not IExternalDataSpec.providedBy(self.context):
+                schema['themes'].required = True
+
+class GeotagMixinEdit(object):
+    """ Edit
+    """
+    adapts(IBaseContent)
+
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def geotag(self):
+        """ Getter
+        """
+        schema = ISchema(self.context)
+        xfield = schema['location']
+        accessor = xfield.getAccessor(self.context)
+        return accessor()
+
+    @geotag.setter
+    def geotag(self, value):
+        """ Setter
+        """
+        schema = ISchema(self.context)
+        xfield = schema['location']
+        mutator = xfield.getMutator(self.context)
+        mutator(value)
+
+
+class IGeotagSingleEdit(Interface):
+    """Interface for editing single location"""
+    geotag = widget.location.GeotagSingleField(
+            title = u"Geotag",
+            description=(u"Geotag: a single geographical location for "
+                         "this content.")
+            )
+
+
+class GeotagSingleEdit(GeotagMixinEdit):
+    """ Single edit
+    """
+    implements(IGeotagSingleEdit)
+
+
+class IGeotagMultiEdit(Interface):
+    """Interface for editing multiple locations"""
+
+    geotag = widget.location.GeotagMultiField(
+            title = u"Geotags",
+            description=(u"Geotags: multiple geographical locations related to"
+                         " this content.")
+            )
+
+
+class GeotagMultiEdit(GeotagMixinEdit):
+    """ Multi Edit
+    """
+    implements(IGeotagMultiEdit)
+
+
+class Subtyper(BaseSubtyper):
+    """We override the default subtyper because of broken logic in its
+    possible_types implementation. The default implementation, due to
+    adapter resolution order, doesn't take into account adapters for
+    IPortalTypedPossibleDescriptors
+
+    Also, not it's possible to hide the menu for content types where
+    it doesn't make sense. This is due to the fact that the subtyper
+    in p4a.video is registered for somewhat generic interfaces
+    (for ex: IATFolder)
+    """
+    _skip = ('Data', 'EEAFigure')
+
+    def possible_types(self, obj):
+        """ Possible types
+        """
+        if obj.portal_type in self._skip:
+            return []
+
+        possible = queryAdapter(obj, IPortalTypedPossibleDescriptors)
+        if possible and possible.possible:
+            return (DescriptorWithName(n, c) for n, c in possible.possible)
+
+        possible = IPossibleDescriptors(obj)
+        return (DescriptorWithName(n, c) for n, c in possible.possible)
+
+
+class TopicVideoContainerDescriptor(BaseTopicVideoContainerDescriptor):
+    """ Topic container
+    """
+    title = _("Video Topic Container")
