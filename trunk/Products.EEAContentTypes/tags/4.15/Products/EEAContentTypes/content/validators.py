fd2e089a28e63a54c549384e25b3460188b7aca5
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-02-06 15:11:53 +0000

    Tagged Products.EEAContentTypes 4.15.

---
+++ trunk/Products.EEAContentTypes/tags/4.15/Products/EEAContentTypes/content/validators.py
@@ -0,0 +1,59 @@
+""" Validators """
+
+from Products.validation.interfaces.IValidator import IValidator
+from Products.validation import validation
+from zope.interface import implements
+import PIL
+
+class ManagementPlanCodeValidator:
+    """ Validator
+    """
+    implements(IValidator)
+
+    def __init__(self,
+                 name,
+                 title='Management plan code',
+                 description='Management plan code validator'):
+        self.name = name
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, *args, **kwargs):
+        """ check if management plan code is in right format """
+        # check we have 3 digits separated by dots
+        errmsg = "Validation failed, management plan code is not in " \
+                 "the format #.#.# e.g. 1.5.2"
+        digits = value[1].split('.')
+        if len(digits) == 3:
+            for dig in digits:
+                try:
+                    int(dig)
+                except ValueError:
+                    return (errmsg)
+            return 1
+        else:
+            return (errmsg)
+
+validation.register(
+    ManagementPlanCodeValidator('management_plan_code_validator'))
+
+
+class ImageMinSize:
+    """ Image minimum size validator
+    """
+    implements(IValidator)
+
+    def __init__( self, name, title='ImageSizeValidator',
+                        description='Image size validator'):
+        self.name = name
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, instance, *args, **kwargs):
+        """ check to see if the image is at least 1024px """
+        image = PIL.Image.open(value)
+        if image.size[0] < 1024:
+            return "Image needs to be at least 1024px in width"
+        return 1
+
+validation.register(ImageMinSize('imageMinSize'))
