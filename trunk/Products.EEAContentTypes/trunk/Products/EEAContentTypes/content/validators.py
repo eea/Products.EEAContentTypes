4acfc5f69e8110ff6ac9be85f104976e8b285265
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-02-24 12:54:55 +0000

    Refs #8948; added youtubeCloudUrlValidator which modified the youtube links and turns them into iframe embedding with extra params

--- trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/content/validators.py
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/content/validators.py
@@ -7,6 +7,9 @@ import PIL
 from cStringIO import StringIO
 from OFS.Image import Pdata
 from Acquisition import aq_base
+import re
+import urllib2
+from Products.Archetypes.interfaces import ISchema
 
 class ManagementPlanCodeValidator:
     """ Validator
@@ -68,3 +71,71 @@ class ImageMinSize:
         return 1
 
 validation.register(ImageMinSize('imageMinSize'))
+
+
+# validator functions used for youtube cloudUrl check for mediacentre
+# and eeacontenttypes
+def youtube_params(vid_url, params = None):
+    """ youtube iframe used for cloudUrl field setter
+    """
+    symbol = "&" if "?" in vid_url else "?"
+    params = params or symbol + "autoplay=1&playnext=1&egm=1&rel=1" \
+                                            "&fs=1&wmode=opaque"
+    return '<iframe width="640" height="360" ' \
+            'src="http://www.youtube.com/embed/%s%s" ' \
+            'frameborder="0" allowfullscreen></iframe>' % (vid_url, params)
+
+def youtube_cloud_validator(value, instance = None):
+    """ check if cloudUrl has a youtube link and construct an iframe out
+    of it if it's just a link
+    """
+    pattern = re.compile('[0-9a-zA-z]{8,100}')
+    obj_schema = ISchema(instance)
+    field = obj_schema['cloudUrl']
+    mutator = field.getMutator(instance)
+
+    if 'youtu.be' in value:
+        # transform youtu.be links iframe code
+        url = urllib2.urlopen(value).url
+        res = pattern.findall(url)
+        if 'list' in url:
+            vid_url = res[0] + '?list=' + res[-1]
+        else:
+            vid_url = res[0]
+        value = youtube_params(vid_url)
+
+    elif 'playlist' in value:
+        # transform playlist link to iframe code
+        res = pattern.findall(value)
+        vid_url = 'videoseries?list=' + res[1]
+        value = youtube_params(vid_url)
+
+    elif ('youtube' in value) and ('iframe' not in value):
+        # transform long youtube link to iframe code
+        res = pattern.findall(value)
+        if 'list' in value:
+            vid_url = res[0] + '?list=' + res[-1]
+        else:
+            vid_url = res[0]
+        value = youtube_params(vid_url)
+
+    mutator(value)
+
+class YoutubeCloudUrlValidator:
+    """ Image minimum size validator
+    """
+    implements(IValidator)
+
+    def __init__( self, name, title='YoutubeCloudUrlValidator',
+                        description='YoutubeCloudUrl Validator'):
+        self.name = name
+        self.title = title or name
+        self.description = description
+
+    def __call__(self, value, instance, *args, **kwargs):
+        """ check and transform links if we don't get iframe code
+        for youtube
+        """
+        youtube_cloud_validator(value, instance)
+
+validation.register(YoutubeCloudUrlValidator('youtubeCloudUrlValidator'))
