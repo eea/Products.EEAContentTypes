6cc834c1ae8415ecb0e4c4d49a5ddaf19f5618dd
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-08-27 13:22:32 +0000

    Refs #9465; added upgrade steps for fixing behaviour of edit form and listing of CloudVideo's which I have already applied on production; Redirect to view when trying to view the cloudFile without /view as previously it tried to download the CloudVideo;

--- trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/content/CloudVideo.py
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/content/CloudVideo.py
@@ -14,15 +14,20 @@ from Products.Archetypes.atapi import (
     registerType
 )
 
+from Products.CMFCore.permissions import View
+
+from Products.LinguaPlone import public
 schema = Schema((
         ImageField('Image',
             required=False,
+            storage = public.AnnotationStorage(migrate=True),
               languageIndependent=True,
             widget=ImageWidget(
                 label='Image',
                 label_msgid='EEAContentTypes_label_image',
                 description_msgid='EEAContentTypes_help_image',
-                i18n_domain='EEAContentTypes'
+                i18n_domain='EEAContentTypes',
+                show_content_type = False
             )
         ),
     ),
@@ -32,7 +37,10 @@ CloudVideo_schema = (getattr(ATFile, 'schema', Schema(())).copy() +
                     ThemeTaggable_schema.copy() +
                     schema.copy())
 
+# hide file field from CloudVideo
 CloudVideo_schema['file'].required = False
+CloudVideo_schema['file'].widget.visible = {"edit" : "invisible", "view":
+                                                                "invisible"}
 
 
 class CloudVideo(ATFile, ThemeTaggable):
@@ -46,7 +54,7 @@ class CloudVideo(ATFile, ThemeTaggable):
     meta_type                  = 'CloudVideo'
     portal_type                = 'CloudVideo'
     allowed_content_types      = [] + list(getattr(ATFile,
-                                                   'allowed_content_types', []))
+                                                'allowed_content_types', []))
     filter_content_types       = 0
     global_allow               = 1
     allow_discussion           = 0
@@ -55,6 +63,13 @@ class CloudVideo(ATFile, ThemeTaggable):
     suppl_views                = ()
     typeDescription            = "CloudVideo"
     typeDescMsgId              = 'description_edit_cloudvideo'
+
+    security.declareProtected(View, 'index_html')
+    def index_html(self, REQUEST=None, RESPONSE=None):
+        """ Redirect to /view for CloudVideo to avoid file download 
+        """
+        return self.REQUEST.response.redirect(
+                                        self.absolute_url() + "/view", 301)
 
     schema = CloudVideo_schema
 
