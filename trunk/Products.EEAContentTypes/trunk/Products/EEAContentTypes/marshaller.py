e9098f117fcf0b98704d54684c86e6f902f343d5
demarant <demarant@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-09-27 13:27:47 +0000

    * Bug fix: fixed some layers not showing up. removed fixed extent.   for gis applications template, now zoom defined on arcgis.com is applied.   [demarant refs #9531]

--- trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/marshaller.py
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/marshaller.py
@@ -6,6 +6,9 @@ from eea.versions.interfaces import IVersionEnhanced
 from eea.versions.versions import get_versions_api
 from zope.component import adapts
 from zope.interface import implements
+from zope.component import getAdapter
+from eea.geotags.interfaces import IGeoTags
+from eea.geotags.storage.interfaces import IGeoTaggable 
 import rdflib
 
 
@@ -28,4 +31,54 @@ class VersioningModifier(object):
                                                         i in api.newest()]
         rdf.dcterms_replaces = [rdflib.URIRef(a['url']) for a in api.oldest()]
 
+        rdf.save()
+        
+        
+class GeoTagRDFModifier(object):
+    """Adds geotags information in rdf
+    """
+
+    implements(ISurfResourceModifier)
+    adapts(IGeoTaggable)
+    #adapts eea.geotags.storage.interfaces.IGeoTagged
+    #or  eea.geotags.storage.interfaces.IGeoTaggable
+    
+    # points with geo positions
+    #
+    # xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#"
+    # <geo:Point>
+    #  <geo:lat>55.701</geo:lat>
+    #  <geo:long>12.552</geo:long>
+    # </geo:Point>
+    #
+    # 
+
+    # Less precise: spatial coverage with DC terms
+    # (using only literals can be missleading
+    # because there are several places with same name)
+    # 
+    # <dct:spatial>Rome</dct:spatial>
+    # 
+    
+    # Precise by pointing to geonames or other resources 
+    # who have exact positions and definitions. we can use dc:subject
+    #
+    # <dct:subject rdf:resource="http://www.geonames.org/630671" />
+    # or
+    # <dct:subject rdf:resource="http://rdfdata.eionet.europa.eu/eea/biogeographic-regions/ALP" />
+    #
+    # or/and (but it looks like dct:spatial is defined to only have literals?)
+    # <dct:spatial rdf:resource="http://www.geonames.org/630671" />
+    #
+    
+    def __init__(self, context):
+        self.context = context
+
+    def run(self, rdf):
+        """change the rdf resource
+        """
+        #import pdb; pdb.set_trace()
+        geo = getAdapter(self.context, IGeoTags)
+        rdf.
+        rdf.dcterms_geotagtest = [rdflib.URIRef('http://rdfdata.eionet.europa.eu/page/ramon/nuts/RO')]
         rdf.save()
