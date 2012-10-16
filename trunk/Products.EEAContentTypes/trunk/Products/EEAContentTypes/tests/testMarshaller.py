0cc72e0aa33844ea87648fa1ec930ea04e00684f
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-10-16 12:43:57 +0000

    Refs #7965; added test for rdf modifiers; cleanup code

---
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/tests/testMarshaller.py
@@ -0,0 +1,42 @@
+""" Tests for Highlight
+"""
+
+from Products.EEAContentTypes.tests.base import EEAContentTypeTestCase
+from eea.versions.versions import createVersion
+import os
+
+
+class testHighlight(EEAContentTypeTestCase):
+    """Test-cases for class(es) ExternalHighlight, Highlight."""
+
+    def afterSetUp(self):
+        """ Set up
+        """
+        self.folder.invokeFactory('Highlight', id="h1", 
+                                   text="Hello world", title="Hightlight 1")
+
+    def test_VersionModifier(self):
+        """ Get publication date
+        """
+        high = self.folder['h1']
+        ver = createVersion(high)
+
+        rdf = high.restrictedTraverse('@@rdf')()
+        self.failUnless ('<dcterms:isReplacedBy rdf:resource="' + 
+               'http://nohost/plone/Members/test_user_1_/h1-1"/>' not in rdf)
+
+        rdf = ver.restrictedTraverse('@@rdf')()
+        self.failUnless('<dcterms:replaces rdf:resource=' + 
+                        '"http://nohost/plone/Members/test_user_1_/h1"/>' not in rdf)
+
+
+def test_suite():
+    """ Suite
+    """
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(testHighlight))
+    return suite
+
+
+
