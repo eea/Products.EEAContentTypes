c4e2ec6a7361513e8d0b8d2dc63a009da85327e1
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-11-09 11:02:57 +0000

    Cleanup relatesToProducts related logic/code, refs #6773

--- trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/catalog.py
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/catalog.py
@@ -12,13 +12,6 @@ def CountReferences(obj):
     try:
         backreferences = IRelations(obj).backReferences()
         fwdreferences = IRelations(obj).forwardReferences()
-
-        #TODO: temporary fix for "relatesToProducts" relation, see #2790
-        if obj.portal_type in ['Data', 'EEAFigure']:
-            backreferences.extend(IRelations(obj).backReferences(
-                relatesTo='relatesToProducts'))
-            fwdreferences.extend(obj.getRelatedProducts())
-
         return len(backreferences) + len(fwdreferences)
     except (TypeError, ValueError):
         # The catalog expects AttributeErrors when a value can't be found
