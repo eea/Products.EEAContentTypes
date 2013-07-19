a80a89a50d451d92b8ef9363d2d7c56c271c2b18
tiberich <tiberiuichim@users.noreply.github.com>
2013-07-19 09:54:09 +0000

    Refs #14824 EEA; added the provenances rdffield adapter

--- Products/EEAContentTypes/marshaller.py
+++ Products/EEAContentTypes/marshaller.py
@@ -9,6 +9,7 @@ from eea.versions.interfaces import IVersionEnhanced, IGetVersions
 from zope.component import adapts
 from zope.interface import implements, Interface
 import rdflib
+import surf
 
 
 class VersioningModifier(object):
@@ -49,3 +50,54 @@ class MediaField2Surf(ATField2Surf):
 
         return [rdflib.URIRef(f.absolute_url()+"/image_xlarge")]
 
+
+class Provenances2Surf(ATField2Surf):
+    """The provenances field
+    """
+    adapts(IField, Interface, ISurfSession)
+
+    name = "wasDerivedFrom"
+    prefix = "prov"
+
+    def __init__(self, *args, **kwargs):
+        super(Provenances2Surf, self).__init__(*args, **kwargs)
+        surf.ns.register(prov="http://www.w3.org/ns/prov#")
+        surf.ns.register(prv = "http://purl.org/net/provenance/ns#")
+        store = self.session.get_default_store()
+        store.reader.graph.bind('prov', surf.ns.PROV, override=True)
+        store.reader.graph.bind('prv', surf.ns.PRV, override=True)
+        store.reader.graph.bind('foaf', surf.ns.FOAF, override=True)
+        store.reader.graph.bind('rdf', surf.ns.RDF, override=True)
+
+    def value(self):
+        """The adapted value for this field
+
+        Desired output:
+
+<prov:wasDerivedFrom> 
+  <prv:DataItem> 
+    <dcterms:title>Title of dataset</dcterms:title> 
+    <foaf:isPrimaryTopicOf rdf:resource="http://organisation/urltodataset"/> 
+    <prov:wasAttributedTo rdf:respurce="uri of owner"/> 
+  </prv:DataItem> 
+</prov:wasDerivedFrom>
+
+        """
+        DataItem = self.session.get_class(surf.ns.PRV.DataItem)
+
+        output = []
+
+        for i, prov in enumerate(self.field.getAccessor(self.context)()):
+            #prov is {'owner': u'http://www.eea.europa.eu', 
+            #         'link': u'http://www.eea.europa.eu/...', 
+            #         'title': u'Corine Land Cover 2000 - 2006 changes'}
+
+            d = DataItem(rdflib.BNode())
+            d.dcterms_title = prov['title']
+            d.foaf_isPrimaryTopicOf = rdflib.URIRef(prov['link'])
+            d.prov_wasAttributedTo = rdflib.URIRef(prov['owner'])
+            d.update()
+
+            output.append(d)
+
+        return output
