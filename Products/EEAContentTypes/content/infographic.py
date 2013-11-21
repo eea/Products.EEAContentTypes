0a61b5cde31cecabd76e30adab8bbc866dd31bf8
ichim-david <ichim.david@gmail.com>
2013-11-21 13:35:41 +0000

    Refs #17505; Changed the following:
    - Inherit Infographic from IEEAContent instead of simply Interface
    - Added the required adapter also for infographic which makes tags, location and
      themes required
    - Removed temporalCoverage since it will be added by the schemaextender now
      that we inherit from IEEAContent

--- Products/EEAContentTypes/content/infographic.py
+++ Products/EEAContentTypes/content/infographic.py
@@ -28,25 +28,7 @@ SCHEMA = atapi.Schema((
             i18n_domain='eea',
         )
     ),
-    atapi.LinesField(
-        name='temporalCoverage',
-        schemata="categorization",
-        languageIndependent=True,
-        required=True,
-        multiValued=1,
-        vocabulary_factory=u"Temporal coverage",
-        widget=atapi.MultiSelectionWidget(
-            macro="temporal_widget",
-            helper_js=("temporal_widget.js",),
-            size=15,
-            label=_("Temporal coverage"),
-            description=_(
-                "The temporal scope of the content of the data "
-                "resource. Temporal coverage will typically include "
-                "a set of years or time ranges."),
-            i18n_domain='eea',
-        )
-    ),
+
     atapi.TextField(
         name='license',
         schemata="creators",
