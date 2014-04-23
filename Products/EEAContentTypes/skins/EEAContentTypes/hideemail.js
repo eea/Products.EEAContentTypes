a24b536b157bd12243d1e253653be215a84d47a4
ichim-david <ichim.david@gmail.com>
2014-04-23 15:14:00 +0000

    * Change: staff page no longer inserts the emails through script tags and document.write which broke the page when jQuery re-evaluated them [ichimdav refs #19369]

--- Products/EEAContentTypes/skins/EEAContentTypes/hideemail.js
+++ Products/EEAContentTypes/skins/EEAContentTypes/hideemail.js
@@ -3,3 +3,15 @@ function create_contact_info_local(theuser,thedomain,linktext) {
         thecontact='<A href="mailto:' + thecontact + '">' + linktext + '</a>';
         return thecontact;
 }
+
+
+jQuery(function($){
+    $(".protectEmail").each(function(){
+        // entry are expected to contain 3 values
+        //  1. email address, 2. domain 3. text content for link
+        var text = this.innerHTML.split(',');
+        this.href = 'mailto:' + text[0] + '@' + text[1];
+        this.innerHTML = text[2];
+        this.className = this.className.replace("hiddenStructure", '');
+    });
+});
