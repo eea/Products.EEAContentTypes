40b29fd806f2a28793e9cfeda6b3beefa499150d
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-06-10 13:07:01 +0000

    Tagged Products.EEAContentTypes 9.0.

---
+++ trunk/Products.EEAContentTypes/tags/9.0/Products/EEAContentTypes/browser/validate.py
@@ -0,0 +1,12 @@
+""" Validate
+"""
+
+from Products.Five import BrowserView
+
+class NotValid(BrowserView):
+    """ Not valid
+    """
+    def validate(self):
+        """ Validate
+        """
+        return False
