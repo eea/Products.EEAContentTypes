f7298de569f326bf98d522bf72e52b725f4a0a8a
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-07-06 12:11:17 +0000

    Tagged Products.EEAContentTypes 10.4.

---
+++ trunk/Products.EEAContentTypes/tags/10.4/Products/EEAContentTypes/content/interfaces.py
@@ -0,0 +1,72 @@
+""" Interfaces
+"""
+from zope.interface import Interface, Attribute
+
+from Products.ATContentTypes.interface.news import IATNewsItem
+from Products.ATContentTypes.interface.folder import IATFolder
+from Products.EEAContentTypes.interfaces import IEEAContent
+from eea.mediacentre.interfaces import IVideo
+
+
+class ICallForTender(Interface):
+    """ Call for tenders
+    """
+
+
+class IQuickEvent(Interface):
+    """ Quick event
+    """
+
+
+class IFlashAnimation(Interface):
+    """ Flash animation
+    """
+    width = Attribute("width of flash file")
+    height = Attribute("height of flash file")
+    bgcolor = Attribute("background color of flash file")
+
+
+class ICloudVideo(IVideo):
+    """  Cloud Video marker interface
+    """
+
+
+class IArticle(IATNewsItem, IATFolder):
+    """ Article
+    """
+
+
+class IExternalHighlight(IATNewsItem, IATFolder):
+    """ External highlight
+    """
+
+
+class IExternalPromotion(IATNewsItem):
+    """ External Promotion
+    """
+
+
+class IInteractiveMap(Interface):
+    """ Marker interface for all interactive maps content
+    """
+
+
+class IInteractiveData(Interface):
+    """ Marker interface for all interactive data viewers (ex pivot tables)
+    """
+
+
+class IGISMapApplication(Interface):
+    """ Marker interface for GIS Map Applications
+    """
+
+
+class IInfographic(IEEAContent):
+    """ Infographic
+    """
+
+
+class IGeoPositioned(Interface):
+    """ Dummy interface added due to #18351. We have old pickled object
+        versions implementing this, and as such, do not remove this interface
+    """
