4b51a75c5570359c450590565a364c8662cde624
lucas <lucas@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-11-11 16:19:29 +0000

    refs #30065 add script to migrate related dataprovenances

---
+++ trunk/Products.EEAContentTypes/trunk/Products/EEAContentTypes/browser/migrate.py
@@ -0,0 +1,41 @@
+""" App module
+"""
+import logging
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+logger = logging.getLogger('Products.EEAContentTypes.browser.migrate')
+
+
+class MigrateDataProvenances(BrowserView):
+    """migrate ds_resolveuid into reference items
+    """
+
+    def __call__(self):
+        """ call
+        """
+        ctool = getToolByName(self.context, 'portal_catalog')
+        brains = ctool(portal_type="DavizVisualization")
+
+        for brain in brains:
+            obj = brain.getObject()
+            field = obj.getField('provenances', None)
+            if not field:
+               continue
+            provenances = field.getAccessor(obj)()
+
+            TOMIGRATE = False
+
+            for p in provenances:
+                link = p.get('link', None)
+                if link and 'ds_resolveuid' in link:
+                    TOMIGRATE = True
+
+            if TOMIGRATE:
+                logger.info("process migrate %s linked by %s" % (
+                    link, obj.absolute_url())
+                )
+                field.set(obj, provenances)
+
+        return "Success!"
+
