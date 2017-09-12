f3649b4727ff1a352c0b9580b580f5eac453c460
ichim-david <ichim.david@gmail.com>
2017-09-12 15:21:29 +0300

    Refs #87691 added languages for each country we support
    - It it a list since a country can have multiple oficial languages and for some countries we might not have eea content translated in that language but we do have for one of the oficial countries.
        This is used by the elastic search app

--- Products/EEAContentTypes/browser/views.py
+++ Products/EEAContentTypes/browser/views.py
@@ -5,6 +5,51 @@ from Products.Five import BrowserView as FiveBrowserView
 import json
 
 
+def get_language_codes():
+    """ get languages for each country we support
+    """
+    return {
+        'Austria': ['de'],
+        'Belgium': ['nl', 'fr', 'de'],
+        'Bulgaria': ['bg'],
+        'Croatia': ['hr'],
+        'Cyprus': ['el', 'tr'],
+        'Czech Republic': ['cs'],
+        'Denmark': ['da'],
+        'Estonia': ['et'],
+        'Finland': ['fi'],
+        'France': ['fr'],
+        'Germany': ['de'],
+        'Greece': ['el'],
+        'Hungary': ['hu'],
+        'Iceland': ['is'],
+        'Ireland': ['ie'],
+        'Italy': ['it'],
+        'Latvia': ['lv'],
+        'Liechtenstein': ['de'],
+        'Lithuania': ['lt'],
+        'Luxembourg': ['de', 'fr'],
+        'Malta': ['mt'],
+        'Norway': ['no'],
+        'Poland': ['pl'],
+        'Portugal': ['pt'],
+        'Romania': ['ro'],
+        'Serbia': ['en'],  # we do not have rs language
+        'Slovakia': ['sk'],
+        'Slovenia': ['sl'],
+        'Spain': ['es'],
+        'Sweden': ['sv'],
+        'Switzerland': ['de', 'fr', 'it'],
+        'Netherlands': ['nl'],
+        'Turkey': ['tr'],
+        'United Kingdom': ['en'],
+        'Bosnia and Herzegovina': ['hr'],
+        'Kosovo': ['hr', 'tr'],
+        'Macedonia': ['hr', 'tr'],
+        'Montenegro': ['hr']
+    }
+
+
 class ViewCountryRegionsJSON(FiveBrowserView):
     """ Json View of each CountryRegionSection
     """
@@ -16,16 +61,21 @@ class ViewCountryRegionsJSON(FiveBrowserView):
         cat = getToolByName(self.context, 'portal_catalog')
         brains = cat(portal_type="CountryRegionSection")
         data = {"items": []}
+        lang_codes = get_language_codes()
+
         for brain in brains:
             obj = brain.getObject()
             url = obj.absolute_url()
+            title = obj.title
             data['items'].append({
-                obj.title: {
+                obj.id: {
                     'url': obj.getRemoteUrl(),
                     'bg_url': url + '/image_panoramic',
                     'flag_url': url + '/flag_mini',
                     'body': obj.getBody(),
-                    'external_links': list(obj.getExternalLinks())
+                    'title': title,
+                    'external_links': list(obj.getExternalLinks()),
+                    'languages': lang_codes.get(title, ['en'])
                 }
             })
         self.request.response.setHeader("Content-type", "application/json")
