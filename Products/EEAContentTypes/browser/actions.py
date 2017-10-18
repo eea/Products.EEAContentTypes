23109cbf11dd5772f2a3c26dc38c79c2879d6dc2
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2017-10-18 10:08:17 +0200

    Refs #88688; Trigger ObjectModifiedEvent when Mark as Country Profile is accessed

--- Products/EEAContentTypes/browser/actions.py
+++ Products/EEAContentTypes/browser/actions.py
@@ -3,8 +3,9 @@
 from Products.EEAContentTypes.content.interfaces import ICountryProfile
 from Products.Five import BrowserView as FiveBrowserView
 from Products.statusmessages.interfaces import IStatusMessage
+from zope.event import notify
 from zope.interface import alsoProvides
-
+from zope.lifecycleevent import ObjectModifiedEvent
 
 class AddCountryProfileInterface(FiveBrowserView):
     """ View
@@ -19,4 +20,5 @@ class AddCountryProfileInterface(FiveBrowserView):
         url = self.context.absolute_url() + '/view'
         IStatusMessage(self.request).addStatusMessage(
             msg, "info")
+        notify(ObjectModifiedEvent(self.context))
         return self.request.response.redirect(url)
