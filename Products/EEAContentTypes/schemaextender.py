b2351eb3eba773b9cd7947b627f49b7fb62f1e70
Alin Voinea <alin.voinea@gmail.com>
2017-11-27 18:21:18 +0200

    Refs #89955 - Register Schema extender/modifer on IEEACommonLayer

--- Products/EEAContentTypes/schemaextender.py
+++ Products/EEAContentTypes/schemaextender.py
@@ -1,14 +1,17 @@
 """ Schema extender
 """
-from archetypes.schemaextender.interfaces import ISchemaModifier
 from zope.interface import implements
+from archetypes.schemaextender.interfaces import ISchemaModifier
+from archetypes.schemaextender.interfaces import IBrowserLayerAwareExtender
+from eea.design.browser.interfaces import IEEACommonLayer
 
 
 class RelatedItemsModifier(object):
     """ Adds the keepReferencesOnCopy feature to all content types
         so related items are not lost when objects are copied.
     """
-    implements(ISchemaModifier)
+    implements(ISchemaModifier, IBrowserLayerAwareExtender)
+    layer = IEEACommonLayer
 
     def __init__(self, context):
         self.context = context
