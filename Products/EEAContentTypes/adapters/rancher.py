b213783f8489520716ee3f720cf134a94163aa01
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-06-14 11:57:14 +0300

    Refs #92952 - Fix caching and optimize rancher status check

--- Products/EEAContentTypes/adapters/rancher.py
+++ Products/EEAContentTypes/adapters/rancher.py
@@ -1,5 +1,6 @@
 """ Utility to check Rancher environment status
 """
+import json
 import logging
 from zope.interface import implements
 from eventlet.green import urllib2
@@ -22,59 +23,27 @@ class RancherStatus(object):
     def get_rancher_metadata(self, url):
         """ Returns Rancher metadata API
         """
-        result = ''
         try:
             with closing(urllib2.urlopen(url, timeout=TIMEOUT)) as conn:
-                result = conn.read()
-        except Exception, err:
+                result = json.loads(conn.read())
+        except Exception as err:
             logger.exception(err)
+            result = []
         return result
 
     def get_stacks(self):
         """ Returns all Rancher stacks from the current environment
         """
         url = "%s/stacks" % RANCHER_METADATA
-        stacks = self.get_rancher_metadata(url)
-        stacks = stacks.split('\n')
-        return stacks
+        request = urllib2.Request(url, headers={"Accept" : "application/json"})
+        return self.get_rancher_metadata(request)
 
-    def get_services(self, stack):
-        """ Returns all services from a specific stack 
-        """
-        url = "%s/stacks/%s/services" % (RANCHER_METADATA, stack)
-        services = self.get_rancher_metadata(url)
-        services = services.split('\n')
-        return services
-
-    def get_service_state(self, stack, service):
-        """ Returns the current state of a specific stack
-        """
-        url = "%s/stacks/%s/services/%s/state" % \
-                        (RANCHER_METADATA, stack, service)
-        state = self.get_rancher_metadata(url)
-        return state
-
-    @cache(lambda *args: MEMCACHE_AGE)
+    @cache(lambda *args: 'rancher-status', lifetime=MEMCACHE_AGE)
     def __call__(self):
-        status = None
-        stacks = self.get_stacks()
-
-        for stack in stacks:
-            services = ''
-            stack_name = stack.split('=')
-            if len(stack_name) > 1:
-                stack_name = stack_name[1]
-                services = self.get_services(stack_name)
-
-                for service in services:
-                    state = ''
-                    service_name = service.split('=')
-                    if len(service_name) > 1:
-                        service_name = service_name[1]
-                        state = self.get_service_state(stack_name,
-                                                       service_name)
-                    if state in ['upgrading', 'upgraded']:
-                        status = 'upgrading'
-                        return status
-
-        return None
+        for stack in self.get_stacks():
+            if stack.get('system'):
+                continue
+            for service in stack.get("services", []):
+                if service.get("state") in ["upgrading"]:
+                    return "upgrading"
+        return ""
