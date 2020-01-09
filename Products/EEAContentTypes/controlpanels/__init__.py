3a77116ba158fca9e2f550e027666775b212e51e
alecghica <alecghica@gmail.com>
2020-01-09 18:32:38 +0000

    added tool in control panel to detect and fix if content is archived without Expiration Date, refs #108916

--- Products/EEAContentTypes/controlpanels/__init__.py
+++ Products/EEAContentTypes/controlpanels/__init__.py
@@ -2,7 +2,10 @@
 """
 from Products.EEAContentTypes.controlpanels.status_effectivedate import \
     StatusEffectiveDate
+from Products.EEAContentTypes.controlpanels.status_expirationdate import \
+    StatusExpirationDate
 
 __all__ = [
-    StatusEffectiveDate.__name__
+    StatusEffectiveDate.__name__,
+    StatusExpirationDate.__name__
 ]
