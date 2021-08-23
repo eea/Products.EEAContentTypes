0612ee821a969b21b63837070eee98d993fa508b
David Ichim <ichim.david@gmail.com>
2021-08-23 21:03:19 +0300

    Refs #128468 calculate image width and height when generating a svg scale:
    - using the same algorithm used by PIL Image.thumbnail we write the calculated
      width and height on plone.scale annotation

--- Products/EEAContentTypes/scale.py
+++ Products/EEAContentTypes/scale.py
@@ -3,6 +3,46 @@
 from plone.app.blob.scale import Blob
 from plone.app.blob.scale import BlobImageScaleFactory
 from plone.app.blob.scale import scaleImage
+from cStringIO import StringIO
+import xml.etree.ElementTree as ET
+
+
+def get_svg_dimensions(data):
+    """ Read width and height from svg either from tag or from viewbox """
+    width = -1
+    height = -1
+    svg = StringIO(data)
+    tree = ET.parse(svg)
+    root = tree.getroot()
+    h = root.attrib.get('height')
+    w = root.attrib.get('width')
+    if not h:
+        viewbox = root.attrib.get('viewBox')
+        if viewbox:
+            values = viewbox.split(' ')
+            w = values[-2]
+            h = values[-1]
+    height = int(h) if h else height
+    width = int(w) if w else width
+    return width, height
+
+
+def calculate_thumbnail_dimensions(data, dimensions):
+    """ Use thumbnail logic from PIL to determine the size of the svg thumbnail
+        https://github.com/python-pillow/Pillow/blob/4.3.x/PIL/Image.py#L2053
+    """
+    svg_width, svg_height = get_svg_dimensions(data)
+    scale_width = dimensions.get('width', 1200)
+    scale_height = dimensions.get('height', 800)
+    scaled_width = 0
+    scaled_height = 0
+    if svg_width > scale_width:
+        scaled_height = int(max(svg_height * scale_width / svg_width, 1))
+        scaled_width = scale_width
+    elif svg_height > scale_height:
+        scaled_width = int(max(svg_width * scale_height / svg_height, 1))
+        scaled_height = scale_height
+    return scaled_width, scaled_height
 
 
 class EEABlobImageScaleFactory(BlobImageScaleFactory):
@@ -13,16 +53,15 @@ class EEABlobImageScaleFactory(BlobImageScaleFactory):
         if wrapper:
             blob = Blob()
             result = blob.open('w')
-            if getattr(wrapper, 'content_type', '')  == 'image/svg+xml':
-                result.write(wrapper.getBlob().open('r').read())
-                dimensions = (
-                    parameters.get('width', 1200),
-                    parameters.get('height', 800)
-                )
-                format = 'svg+xml'
+            if getattr(wrapper, 'content_type', '') == 'image/svg+xml':
+                data = wrapper.getBlob().open('r').read()
+                dimensions = calculate_thumbnail_dimensions(data, parameters)
+                result.write(data)
+                file_format = 'svg+xml'
             else:
-                _, format, dimensions = scaleImage(wrapper.getBlob().open('r'),
+                _, file_format, dimensions = scaleImage(
+                    wrapper.getBlob().open('r'),
                     result=result, **parameters)
 
             result.close()
-            return blob, format, dimensions
+            return blob, file_format, dimensions
